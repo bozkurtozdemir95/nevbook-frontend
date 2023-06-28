4ce07b97cfc1d2bc6af18915f8d9c2c7b029d80e
bozkurtozdemir95 <bozkurtozdemir95@gmail.com>
2023-06-29 00:37:07 +0200

    book details basic ui

--- src/app/pages/book/book.component.ts
+++ src/app/pages/book/book.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import productData from '../../data/products.json';
 
 @Component({
   selector: 'app-book',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./book.component.scss']
 })
 export class BookComponent {
+  products = productData;
+  book : any;
+  quantity = 1;
 
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe(params => {
+
+      this.products.find((e: any) => {
+        if(e.productID === +params['id']){
+          this.book = e;
+        }
+        console.log(this.book);
+      })
+    });
+  }
+
+  minus() {
+    this.quantity > 1 ? this.quantity = this.quantity - 1 : this.quantity = 1;
+  }
+
+  plus() {
+    this.quantity = this.quantity + 1;
+  }
 }
